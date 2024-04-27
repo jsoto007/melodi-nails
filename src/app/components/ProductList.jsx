6225419ce3838d4af18353f8745903835ad157c9
Jose Soto <jsoto7087@gmail.com>
2024-04-27 16:20:35 -0400

    adds product List

--- src/app/components/ProductList.jsx
+++ src/app/components/ProductList.jsx
@@ -1,6 +1,56 @@
 import React from 'react'
 
 export default function ProductList() {
+
+  const products = [
+    {
+    name: "Russian Minicure",
+    price: "50",
+    description: "We use a proprietaty system to apply acrillic nail polish and dry it."
+    },
+    {
+    name: "Gel Pedicure",
+    price: "45",
+    description: "We use a proprietaty system to apply acrillic nail polish and dry it."
+    },
+    {
+    name: "Acrylic Full Set",
+    price: "65",
+    description: "We use a proprietaty system to apply acrillic nail polish and dry it."
+    },
+    {
+    name: "Fill In Acrylic Color",
+    price: "55",
+    description: "We use a proprietaty system to apply acrillic nail polish and dry it."
+    },
+    {
+    name: "Acrylic Clear & Polish Gel",
+    price: "50",
+    description: "We use a proprietaty system to apply acrillic nail polish and dry it."
+    },
+    {
+    name: "Fill In Clear",
+    price: "40",
+    description: "We use a proprietaty system to apply acrillic nail polish and dry it."
+    },
+    {
+    name: "Ombré Nails",
+    price: "70",
+    description: "We use a proprietaty system to apply acrillic nail polish and dry it."
+    },
+    {
+    name: "Perfect French",
+    price: "90",
+    description: "We use a proprietaty system to apply acrillic nail polish and dry it."
+    },
+    {
+    name: "Soack Off",
+    price: "10",
+    description: "We use a proprietaty system to apply acrillic nail polish and dry it."
+    },
+  ]
+
+
   return (
     <div className='bg-[#ECE7E2]'>
     <div className='md:mx-16 lg:mx-16'>
@@ -10,18 +60,24 @@ export default function ProductList() {
       </div>
       <div className="mt-6 border-t border-gray-100">
         <dl className="divide-y divide-gray-100">
-        
-          <div className="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
-            <dt className="text-sm font-medium leading-6 text-gray-900">Application for</dt>
-            <dd className="mt-1 flex text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0">
-              <span className="flex-grow">Backend Developer</span>
-              <span className="ml-4 flex-shrink-0">
-                <button type="button" className="rounded-md bg-white font-medium text-indigo-600 hover:text-indigo-500">
-                  Update
-                </button>
-              </span>
-            </dd>
-          </div>
+          {
+            products.map(()=> {
+              return(
+
+                <div className="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
+                  <dt className="text-sm font-medium leading-6 text-gray-900">Application for</dt>
+                  <dd className="mt-1 flex text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0">
+                    <span className="flex-grow">Backend Developer</span>
+                    <span className="ml-4 flex-shrink-0">
+                      <button type="button" className="rounded-md bg-white font-medium text-indigo-600 hover:text-indigo-500">
+                        Update
+                      </button>
+                    </span>
+                  </dd>
+                </div>
+              )
+            })
+          }
         
        
         </dl>
