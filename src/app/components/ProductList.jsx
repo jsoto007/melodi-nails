7a9a90c809e67daaae8dd046bd67975102481102
Jose Soto <jsoto7087@gmail.com>
2024-05-08 11:33:40 -0400

    updates descriptions for products

--- src/app/components/ProductList.jsx
+++ src/app/components/ProductList.jsx
@@ -8,55 +8,67 @@ export default function ProductList() {
     id: 1,
     name: "Russian Manicure",
     price: "50",
-    description: "We use a proprietaty system to apply acrillic nail polish and dry it."
+    description: "Consta de una manicura en seco donde nos enfocamos en la limpieza de las cutículas y pulido de la piel, más un esmaltado semi permanente y gel calcio."
     },
     {
       id: 2,
     name: "Gel Pedicure",
     price: "45",
-    description: "We use a proprietaty system to apply acrillic nail polish and dry it."
+    description: "Una limpieza de uñas y pies , exfoliación , hidratación y el esmaltado de su preferencia semi permanent."
     },
     {
       id: 3,
     name: "Acrylic Full Set",
     price: "65",
-    description: "We use a proprietaty system to apply acrillic nail polish and dry it."
+    description: "Consta de un acrílico de color ya sea el de su preferencia. "
     },
     {
     id: 4,
     name: "Fill In Acrylic Color",
     price: "55",
-    description: "We use a proprietaty system to apply acrillic nail polish and dry it."
+    description: "Es un relleno en el área ya crecida de las uñas que ya lleva puesta del mismo color."
     },
     {
     id: 5,
     name: "Acrylic Clear & Polish Gel",
     price: "50",
-    description: "We use a proprietaty system to apply acrillic nail polish and dry it."
+    description: "Uñas en acrílico trasparente y un esmaltado semi permanente."
     },
     {
     id: 6,
     name: "Fill In Clear",
     price: "40",
-    description: "We use a proprietaty system to apply acrillic nail polish and dry it."
+    description: "Relleno de acrílico cristal y un esmaltado semi permanente"
     },
     {
     id: 7,
     name: "Ombré Nails",
     price: "70",
-    description: "We use a proprietaty system to apply acrillic nail polish and dry it."
+    description: "Consta de un difuminado entre dos acrílicos de colores."
     },
     {
     id: 8,
     name: "Perfect French",
     price: "90",
-    description: "We use a proprietaty system to apply acrillic nail polish and dry it."
+    description: "Este set consta de un fresh y una base en acrílico de color."
     },
     {
     id: 9,
     name: "Soack Off",
     price: "10",
-    description: "We use a proprietaty system to apply acrillic nail polish and dry it."
+    description: "Remojamos las uñas en acetona pura al 100% y agua caliente para retiro de uñas."
+    },
+    {
+    id: 10,
+    name: "Acrylic full set + French",
+    price: "75",
+    description: "Acrílico de color más French del color de su preferencia en gel."
+    },
+    {
+    id: 11,
+    name: "Paraffin treatment",
+    price: "15",
+    description: "En este tratamiento les sumergiremos  sus pies o manos a una cera caliente que le ayudará con la hidratación y circulación."
     },
   ]
 
@@ -80,7 +92,7 @@ export default function ProductList() {
                     <span className="flex-grow">{product.description}</span>
                     <span className="ml-4 flex-shrink-0">
                       <span className="rounded-md font-bold text-grey-900">
-                        $ {product.price}
+                        $ {product.price} <span className='text-xs font-light'>& Up</span>
                       </span>
                     </span>
                   </dd>
