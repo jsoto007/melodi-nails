b2550f3bd888b9ff7fb81b51d9346fbd082080ee
Jose Soto <jsoto7087@gmail.com>
2024-05-11 11:44:41 -0400

    updates

--- src/app/page.js
+++ src/app/page.js
@@ -3,12 +3,14 @@ import React from "react";
 import Hero from "./components/Hero";
 import ProductList from "./components/ProductList";
 import CalendarContainer from "./components/CalendarContainer";
+import PreviewPage from "./components/PreviewPage";
 
 export default function Home() {
   return (
    <div>
     <Hero />
     <ProductList />
+    <PreviewPage />
     <CalendarContainer />
    </div>
   );
