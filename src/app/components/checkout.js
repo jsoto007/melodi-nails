0bbf5cf694d2d7d824910c4d4d294273baf9e51f
Jose Soto <jsoto7087@gmail.com>
2024-05-13 01:34:46 -0400

    adds payment

---
+++ src/app/components/checkout.js
@@ -0,0 +1,24 @@
+import { loadStripe } from "@stripe/stripe-js";
+
+
+export async function checkout({ lineItems }){
+  
+  let stripePromise = null 
+
+  const getStripe = () => {
+    if(!stripePromise) {
+      stripePromise = loadStripe(process.env.NEXT_PUBLIC_API_KEY)
+    }
+    return stripePromise;
+  }
+
+  const stripe = await getStripe()
+
+  await stripe.redirectToCheckout({
+    mode: 'payment',
+    lineItems,
+    successUrl: `https://melodinails.com/#booking`,
+    cancelUrl: window.location.origin
+
+  })
+}
