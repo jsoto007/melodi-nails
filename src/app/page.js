c84a35c18532e0026e9936de16c6111ca2a34e76
Jose Soto <jsoto7087@gmail.com>
2024-05-21 20:51:34 -0400

    adds insta'

--- src/app/page.js
+++ src/app/page.js
@@ -4,6 +4,7 @@ import Hero from "./components/Hero";
 import ProductList from "./components/ProductList";
 import CalendarContainer from "./components/CalendarContainer";
 import PreviewPage from "./components/PreviewPage";
+import Footer from "./components/Footer";
 
 export default function Home() {
   return (
@@ -12,6 +13,7 @@ export default function Home() {
     <ProductList />
     <PreviewPage />
     <CalendarContainer />
+    <Footer />
    </div>
   );
 }
